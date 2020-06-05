3558b9c0eba5d351b516cf46183647c2a704ed42
Atila Silva <atila__7@hotmail.com>
2020-06-05 20:32:15 -0300

    Projeto criado

---
+++ src/main/java/com/gcmmogi/gcm/GcmApplication.java
@@ -0,0 +1,13 @@
+package com.gcmmogi.gcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gcmmogi/gcm/GcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcmmogi.gcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
