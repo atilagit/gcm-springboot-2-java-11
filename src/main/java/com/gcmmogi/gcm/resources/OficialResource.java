6e307fd6848660229d9af90c9bc06161353a52be
Atila Silva <atila__7@hotmail.com>
2020-06-06 00:36:54 -0300

    Camada de serviço, registro de componentes

--- src/main/java/com/gcmmogi/gcm/resources/OficialResource.java
+++ src/main/java/com/gcmmogi/gcm/resources/OficialResource.java
@@ -1,20 +1,33 @@
 package com.gcmmogi.gcm.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gcmmogi.gcm.entities.Oficial;
-import com.gcmmogi.gcm.entities.enums.Posto;
+import com.gcmmogi.gcm.services.OficialService;
 
 @RestController
 @RequestMapping(value = "/oficiais")
 public class OficialResource {
 	
+	@Autowired
+	private OficialService service;
+	
 	@GetMapping
-	public ResponseEntity<Oficial> findAll(){
-		Oficial o = new Oficial(1L, "joao87", "123", "Joao", "Time-A", 5, Posto.EM_CAMPO);
-		return ResponseEntity.ok().body(o);
+	public ResponseEntity<List<Oficial>> findAll(){
+		List<Oficial> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Oficial> findById(@PathVariable Long id){
+		Oficial obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/gcmmogi/gcm/services/OficialService.java
@@ -0,0 +1,26 @@
+package com.gcmmogi.gcm.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gcmmogi.gcm.entities.Oficial;
+import com.gcmmogi.gcm.repositories.OficialRepository;
+
+@Service
+public class OficialService {
+	
+	@Autowired
+	private OficialRepository repository;
+	
+	public List<Oficial> findAll(){
+		return repository.findAll();
+	}
+	
+	public Oficial findById(Long id) {
+		Optional<Oficial> obj = repository.findById(id);
+		return obj.get();
+	}
+}
