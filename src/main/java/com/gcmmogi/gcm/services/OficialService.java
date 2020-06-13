9ec0fb875bb870b5519420a74015778b59a86783
Atila Silva <atila__7@hotmail.com>
2020-06-13 13:43:53 -0300

    Atualização de Oficial

--- src/main/java/com/gcmmogi/gcm/services/OficialService.java
+++ src/main/java/com/gcmmogi/gcm/services/OficialService.java
@@ -31,4 +31,18 @@ public class OficialService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public Oficial update(Long id, Oficial obj) {
+		Oficial entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(Oficial entity, Oficial obj) {
+		entity.setLogin(obj.getLogin());
+		entity.setNome(obj.getNome());
+		entity.setTime(obj.getTime());
+		entity.setViatura(obj.getViatura());
+		entity.setPosto(obj.getPosto());
+	}
 }
