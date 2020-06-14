782c2c2f0e4f429c4ce0b9331c1312fe910aec02
Atila Silva <atila__7@hotmail.com>
2020-06-13 21:53:43 -0300

    Exception handling - delete

--- src/main/java/com/gcmmogi/gcm/services/OficialService.java
+++ src/main/java/com/gcmmogi/gcm/services/OficialService.java
@@ -4,8 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import com.gcmmogi.gcm.services.exceptions.DatabaseException;
 import com.gcmmogi.gcm.entities.Oficial;
 import com.gcmmogi.gcm.repositories.OficialRepository;
 import com.gcmmogi.gcm.services.exceptions.ResourceNotFoundException;
@@ -30,7 +33,13 @@ public class OficialService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public Oficial update(Long id, Oficial obj) {
