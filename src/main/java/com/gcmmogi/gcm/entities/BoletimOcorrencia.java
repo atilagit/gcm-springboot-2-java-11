b9026233571a17dbf449e7e5895d3d670520a2b5
Atila Silva <atila__7@hotmail.com>
2021-01-22 08:50:03 -0300

    Melhoria no campo de relatórioDaGCM

--- src/main/java/com/gcmmogi/gcm/entities/BoletimOcorrencia.java
+++ src/main/java/com/gcmmogi/gcm/entities/BoletimOcorrencia.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -44,6 +45,8 @@ public class BoletimOcorrencia implements Serializable {
 	private Integer kmPrimeiroTermino;
 	private Integer kmSegundoTermino;
 	private String local;
+	
+	@Column(columnDefinition = "TEXT")
 	private String relatorioDaGCM;
 
 	@ManyToOne
